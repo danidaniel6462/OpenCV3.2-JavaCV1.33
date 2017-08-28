563983b877ea56dfef6e3af9d527253bbddfdebe
danidaniel6462 <darwindaniel.dl@gmail.com>
2017-08-28 13:14:37 -0500

    Básico - Procesamiento Video
    - Programa básico para procesamiento de Video
    - Ejemplo con video en escala de gris

--- src/futuroingeniero/engineTest/MainCaptureFrame.java
+++ src/futuroingeniero/engineTest/MainCaptureFrame.java
@@ -5,8 +5,14 @@ package futuroingeniero.engineTest;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
+
+import org.bytedeco.javacpp.opencv_videoio.CvCapture;
+
 import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import static org.bytedeco.javacpp.opencv_objdetect.*;
+
+import static org.bytedeco.javacpp.opencv_videoio.*;
 
 /**
  * @author Daniel Loza
@@ -19,31 +25,40 @@ public class MainCaptureFrame {
 	 */
 	public static void main(String[] args) {
 	
-		/*
-		IplImage img = cvLoadImage("res/uce.png");
-		cvShowImage("Hola funciona¿", img);
-		cvSmooth(img, img, CV_GAUSSIAN, 13, 0, 0, 0);
-		cvShowImage("BlurImage", img);
-		cvWaitKey();
-		cvReleaseImage(img);
-		*/
+		// creo una variable captura para guardar la información del video que se carga en la webCam
+		CvCapture captura = cvCreateCameraCapture(CV_CAP_ANY);
+		// los videos están constituidos de mínimo 24 cuadros por segundo
+		// así que se crea una variable de tipo IplImage para guardar cada cuadro del video
+		IplImage frame;
+		// creo una imagen con grises
+		IplImage grayImg = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
 		
-		IplImage img = cvLoadImage("res/uce.png");
-		IplImage hsvImg = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 3);
-		IplImage grayImg = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
+		// creo una ventana para mostrar la captura del video
+		cvNamedWindow("Video", CV_WINDOW_AUTOSIZE);
 		
-		cvCvtColor(img, hsvImg, CV_BGR2HSV);
-		cvCvtColor(img, grayImg, CV_BGR2GRAY);
+		while(true){
+			// captura cada frame del video
+			frame = cvQueryFrame(captura);
+			// comprobamos que tenga un frame para no salirdel bucle infinito 
+			if(frame == null){
+				System.err.println("ERROR: No hay video que mostrar");
+				break;
+			}
+			// calcula la imagen como espejo, el valor 1 representa que será en forma horizontal 
+			cvFlip(frame, frame, 1);
+			cvCvtColor(frame, grayImg, CV_BGR2GRAY);
+			
+			//Mostramos en una ventana los frame del video
+			cvShowImage("Video", grayImg);
+			// controla la velocidad de reproducción de los videos controlado en milisegundos 
+			char c = (char) cvWaitKey(1);
+			// si se presiona la tecla ESC el video finaliza
+			if(c == 27) break;
+		}
 		
-		cvShowImage("Original", img);
-		cvShowImage("HSV IMAGE", hsvImg);
-		cvShowImage("GRAY IMAGE", grayImg);
-		
-		cvWaitKey();
-		cvReleaseImage(img);
-		cvReleaseImage(hsvImg);
-		cvReleaseImage(grayImg);
-		
+		// liberar memoria del computador
+		cvReleaseCapture(captura);
+		// cerrar la ventana del video
+		cvDestroyWindow("Video");
 	}
-
 }
