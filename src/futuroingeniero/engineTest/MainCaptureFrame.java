47858ad78fbf4366fb60bb04a351217b62d7b39c
danidaniel6462 <darwindaniel.dl@gmail.com>
2017-08-28 15:44:10 -0500

    Filtrando Color
    - Obteniendo la imagen a través de la cámara web y filtrando el color azul

--- src/futuroingeniero/engineTest/MainCaptureFrame.java
+++ src/futuroingeniero/engineTest/MainCaptureFrame.java
@@ -7,12 +7,16 @@ import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 import org.bytedeco.javacpp.opencv_videoio.CvCapture;
+import org.bytedeco.javacv.FrameRecorder;
+import org.bytedeco.javacv.FrameRecorder.Exception;
+import org.bytedeco.javacv.OpenCVFrameRecorder;
 
 import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_objdetect.*;
 
 import static org.bytedeco.javacpp.opencv_videoio.*;
+import static org.bytedeco.javacpp.avcodec.*;
 
 /**
  * @author Daniel Loza
@@ -24,41 +28,44 @@ public class MainCaptureFrame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
-		// creo una variable captura para guardar la información del video que se carga en la webCam
-		CvCapture captura = cvCreateCameraCapture(CV_CAP_ANY);
-		// los videos están constituidos de mínimo 24 cuadros por segundo
-		// así que se crea una variable de tipo IplImage para guardar cada cuadro del video
-		IplImage frame;
-		// creo una imagen con grises
-		IplImage grayImg = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
 		
-		// creo una ventana para mostrar la captura del video
-		cvNamedWindow("Video", CV_WINDOW_AUTOSIZE);
+		IplImage imgOrigen, imgHsv, imgBin;
+		CvCapture cap = cvCreateCameraCapture(CV_CAP_ANY);
+		// crear la imagen vacía del tamaño de la imagen original
+		// de 8 bits y 3 canales
+		imgHsv = cvCreateImage(cvSize(640, 480), 8, 3);
+		// 1 canal xq será imagen binarizada
+		imgBin = cvCreateImage(cvSize(640, 480), 8, 1);
+		
+
 		
 		while(true){
-			// captura cada frame del video
-			frame = cvQueryFrame(captura);
-			// comprobamos que tenga un frame para no salirdel bucle infinito 
-			if(frame == null){
-				System.err.println("ERROR: No hay video que mostrar");
-				break;
-			}
-			// calcula la imagen como espejo, el valor 1 representa que será en forma horizontal 
-			cvFlip(frame, frame, 1);
-			cvCvtColor(frame, grayImg, CV_BGR2GRAY);
+			imgOrigen = cvQueryFrame(cap);
 			
-			//Mostramos en una ventana los frame del video
-			cvShowImage("Video", grayImg);
-			// controla la velocidad de reproducción de los videos controlado en milisegundos 
-			char c = (char) cvWaitKey(1);
-			// si se presiona la tecla ESC el video finaliza
+			if(imgOrigen == null){ break; }
+			
+			cvCvtColor(imgOrigen, imgHsv,CV_BGR2HSV);
+			
+			// rango de colores mín y máx para binarizar la imagen 
+			CvScalar minC = cvScalar(95, 150, 75, 0);
+			CvScalar maxC = cvScalar(145, 255, 255, 0);
+			
+			// calcula la imagen binarizada a partir de los parámetros ingresados
+			cvInRangeS(imgHsv, minC, maxC, imgBin);
+			
+			// muestra la imagen
+			cvShowImage("Origen", imgOrigen);
+			cvShowImage("Imagen Binaria", imgBin);
+			
+			// deja abierta las ventanas hasta que se cierren
+			char c = (char) cvWaitKey(30);
+			
 			if(c == 27) break;
+			
 		}
+		cvReleaseCapture(cap);
 		
-		// liberar memoria del computador
-		cvReleaseCapture(captura);
-		// cerrar la ventana del video
-		cvDestroyWindow("Video");
+		cvDestroyWindow("Origen");
+		cvDestroyWindow("Imagen Binaria");
 	}
 }
